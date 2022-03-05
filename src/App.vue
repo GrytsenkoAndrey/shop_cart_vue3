abdfb4fc85da0e5ccc153e1b23ff17a8ebf67335
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-05 17:32:59 +0200

    Add Cart route & component

--- src/App.vue
+++ src/App.vue
@@ -4,116 +4,64 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class='text-center'>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" :class="{active: $router.name === 'Home'}">Home</RouterLink>
+        <RouterLink to="/cart" :class="{active: $router.name === 'Cart'}">Cart</RouterLink>
       </nav>
     </div>
   </header>
 
+  <div class='container'>
   <RouterView />
+  </div>
+  
 </template>
 
-<style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+body {
+  background-color: rgba(245, 245, 245, .7);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#nav {
+  background-color: white;
+  padding: 10px;
+  line-height: 30px;
+  height: 30px;
+  width:100%;
 }
 
-a,
-.green {
+a {
+  font-weight: bold;
+  color: garkgray;
   text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
+  margin: 0 5px;
+  font-size: 1.25rem;
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
+  &.active {
+    color: #2c3e50;
   }
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.text-center {
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  display: block;
+  width: 90%;
+  padding: 10px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/views/CartView.vue
@@ -0,0 +1,3 @@
+<template>
+<p>this is the CartView</p>
+</template>
